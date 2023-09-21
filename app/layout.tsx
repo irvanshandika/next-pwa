a28e39805e86a291e4668c6f317083410a2b9ac0
irvanshandika <shandikamuhammadirvan@gmail>
2023-09-21 23:16:00 +0700

    fix theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "Next PWA",
   description: "Next.js PWA template",
   manifest: "/manifest.json",
-  // viewport: "width=device-width, initial-scale=1",
+  themeColor: "#000000",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
